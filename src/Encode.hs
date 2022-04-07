1d5890d463eb22c7ba81efef039d61aaf6c8e9e9
fermatspace <78305500+fermatspace@users.noreply.github.com>
2022-04-07 15:29:16 +0200

    Update Encode.hs

--- src/Encode.hs
+++ src/Encode.hs
@@ -1,22 +1,3 @@
---{-# LANGUAGE FlexibleInstances #-}
---module Serialize where
---
---import Prelude
---import Cards
---import qualified Data.ByteString.Lazy as LBS
---import qualified Data.ByteString.Char8 as BS
---import Data.Char (chr)
---import Text.Read
---
---cardToBS :: Card -> BS.ByteString
---cardToBS card = BS.pack $ show $ card
---
---bsToCard :: BS.ByteString -> Maybe Card
---bsToCard bs = readMaybe $ map (chr . fromEnum) . BS.unpack $ bs :: Maybe Card
---
---encodeDeck :: Deck -> [BS.ByteString]
---encodeDeck deck = map cardToBS deck
-
 {-# LANGUAGE FlexibleInstances #-}
 module Encode where
 
